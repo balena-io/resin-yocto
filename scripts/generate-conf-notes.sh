0ca78ff185ca7c6ccb60263bc45e618eb86abe76
Theodor Gherzan <theodor@resin.io>
2015-05-31 18:20:51 +0200

    Use description of boards from resin-device-types
    Signed-off-by: Theodor Gherzan <theodor@resin.io>

--- scripts/generate-conf-notes.sh
+++ scripts/generate-conf-notes.sh
@@ -27,15 +27,15 @@ echo -e "
  ---------------------------------- \n" > $CONF/$CONFNAME
 
 echo "Resin specific targets are:" >> $CONF/$CONFNAME
-for target in `cat $JSON | jq  -r '[.supported[].image] | unique | .[]'`; do
+for target in `cat $JSON | jq  -r '[.[].yoctoimage] | unique | sort | .[] | select( . != null)'`; do
     echo "    $target" >> $CONF/$CONFNAME
 done
 echo >> $CONF/$CONFNAME
 
-for machine in `cat $JSON | jq  -r '.supported[].machine'`; do
-    NAME=`cat $JSON | jq  -r '.supported[] | select(.machine == '\"${machine}\"').name'`
-    MACHINE=`cat $JSON | jq  -r '.supported[] | select(.machine == '\"${machine}\"').machine'`
-    IMAGE=`cat $JSON | jq  -r '.supported[] | select(.machine == '\"${machine}\"').image'`
+for machine in `cat $JSON | jq  -r '[.[].yoctomachine] | sort | .[] | select( . != null)'`; do
+    NAME=`cat $JSON | jq  -r '.[] | select(.yoctomachine == '\"${machine}\"').name'`
+    MACHINE=`cat $JSON | jq  -r '.[] | select(.yoctomachine == '\"${machine}\"').yoctomachine'`
+    IMAGE=`cat $JSON | jq  -r '.[] | select(.yoctomachine == '\"${machine}\"').yoctoimage'`
     printf "%-25s : %s\n" "$NAME" "\$ MACHINE=$MACHINE bitbake $IMAGE" >> $CONF/$CONFNAME
 done
 echo >> $CONF/$CONFNAME
